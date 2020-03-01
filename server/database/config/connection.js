8a63fd1bf736b541bb6d78518c1c1f1be7bba805
rehabas <reremo45@gmail.com>
2020-03-01 09:49:08 +0200

    test

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,7 +1,15 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
 
-if (!process.env.DB_URL) throw new Error('No Database URL!!!');
+let DBUrldbUrl;
+
+if (process.env.NODE_ENV === 'production') {
+  dbUrl = process.env.DATABASE_URL;
+} else if (process.env.NODE_ENV === 'development') {
+  dbUrl = process.env.DB_URL;
+}
+//if (!process.env.DB_URL) throw new Error('No Database URL!!!');
+
 
 const options = {
   connectionString : process.env.DB_URL,
